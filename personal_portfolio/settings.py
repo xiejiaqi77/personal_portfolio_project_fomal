7669aa26713d22e2807a7b732154b58a808dcc46
xiejiaqi77 <xiejiaqi77.job@gmail.com>
2021-02-27 05:25:26 +0000

    remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'b%&rma7e(m@ob%1tsv=fx$+w^*48s&7=t6au4dda7=s@xntyy2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xiejiaqi77.pythonanywhere.com']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'  # this can be modifid
 MEDIA_ROOT = BASE_DIR / 'media'   # must be 'media' instead of db.sqlite
